e5b411e061f7ae8844f507e7a4c9874901420a1e
Nathan Yang <nathanyang.w@outlook.com>
2026-06-18 08:47:18 +0200

    Add structure to Pastries component

--- src/components/Pastries/Pastries.tsx
+++ src/components/Pastries/Pastries.tsx
@@ -1,11 +1,31 @@
 // src/components/Pastries/Pastries.tsx
 
+import { ConsumableType } from "../../data/consumable";
+import { consumables } from "../../data/consumables";
 import styles from "./Pastries.module.css";
 
 export default function Pastries() {
   return (
     <section id="pastries" className={styles.pastries}>
       <h1>Pastries</h1>
+
+      <div className={styles.pastriesContainer}>
+        {consumables
+          .filter((pastry) => pastry.type === ConsumableType.VIENNOISERIE)
+          .map((pastry) => (
+            <div className="consumable" key={pastry.id}>
+              <img
+                src={pastry.image}
+                alt={pastry.name}
+                className={styles.pastryShowcase}
+              />
+              <ul>
+                <li>{pastry.name}</li>
+                <li>{pastry.price} €</li>
+              </ul>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }

e031ffc880789ab6fb5c580273240a096feb4b85
Nathan Yang <nathanyang.w@outlook.com>
2026-06-18 08:31:39 +0200

    Add coffees div to Coffees component

--- src/components/Coffees/Coffees.tsx
+++ src/components/Coffees/Coffees.tsx
@@ -1,5 +1,7 @@
 // src/components/Coffees/Coffees.tsx
 
+import { ConsumableType } from "../../data/consumable";
+import { consumables } from "../../data/consumables";
 import styles from "./Coffees.module.css";
 
 export default function Coffees() {
@@ -7,7 +9,23 @@ export default function Coffees() {
     <section id="coffees" className={styles.coffees}>
       <h1>Coffees</h1>
 
-      <div className="consumables-container"></div>
+      <div className={styles.consumablesContainer}>
+        {consumables
+          .filter((coffee) => coffee.type === ConsumableType.DRINK)
+          .map((coffee) => (
+            <div className="consumable" key={coffee.id}>
+              <img
+                src={coffee.image}
+                alt={coffee.name}
+                className={styles.consumableShowcase}
+              />
+              <ul>
+                <li>{coffee.name}</li>
+                <li>{coffee.price} €</li>
+              </ul>
+            </div>
+          ))}
+      </div>
     </section>
   );
 }
---
+++ src/data/consumables.ts
@@ -0,0 +1,37 @@
+// src/data/consumables.ts
+
+import { ConsumableType, Consumable } from "./consumable.ts";
+
+import expressoImg from "../assets/consumables/expresso.jpg";
+import cappuccinoImg from "../assets/consumables/cappuccino.jpg";
+import latteImg from "../assets/consumables/latte.jpg";
+
+import quicheImg from "../assets/consumables/quiche.jpg";
+import croqueMonsieurImg from "../assets/consumables/croque-monsieur.jpg";
+import cesarSaladImg from "../assets/consumables/cesar-salad.jpg";
+
+import croissantImg from "../assets/consumables/croissant.jpg";
+import chocolateCroissantImg from "../assets/consumables/chocolate-croissant.jpg";
+import briocheImg from "../assets/consumables/brioche.jpg";
+
+export const consumables: Consumable[] = [
+  new Consumable(expressoImg, "Expresso", ConsumableType.DRINK, 1.0),
+  new Consumable(cappuccinoImg, "Cappuccino", ConsumableType.DRINK, 3.0),
+  new Consumable(latteImg, "Latte", ConsumableType.DRINK, 3.5),
+  new Consumable(quicheImg, "Quiche", ConsumableType.FOOD, 4.0),
+  new Consumable(
+    croqueMonsieurImg,
+    "Croque-Monsieur",
+    ConsumableType.FOOD,
+    4.0,
+  ),
+  new Consumable(cesarSaladImg, "Cesar Salad", ConsumableType.FOOD, 8.0),
+  new Consumable(croissantImg, "Croissant", ConsumableType.VIENNOISERIE, 4.0),
+  new Consumable(
+    chocolateCroissantImg,
+    "Chocolate Croissant",
+    ConsumableType.VIENNOISERIE,
+    1.0,
+  ),
+  new Consumable(briocheImg, "Brioche", ConsumableType.VIENNOISERIE, 1.0),
+];

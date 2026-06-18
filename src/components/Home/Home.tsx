c7671dd25b389330cc544dacea3ff28ce7eff068
Nathan Yang <nathanyang.w@outlook.com>
2026-06-18 10:58:37 +0200

    Adjust style of h1

--- src/components/Home/Home.tsx
+++ src/components/Home/Home.tsx
@@ -5,7 +5,7 @@ import styles from "./Home.module.css";
 export default function Home() {
   return (
     <section id="home" className={styles.home}>
-      <h1 className={styles.welcome}>Welcome at Café</h1>
+      <h1>Welcome at Café</h1>
     </section>
   );
 }
--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -14,14 +14,18 @@ type NavBarProps = {
 export default function NavBar({ navigationLinks }: NavBarProps) {
   return (
     <nav className={styles.navbar}>
-      {navigationLinks.map((navigationLink) => (
-        <a
-          href={"#" + navigationLink.name.toLocaleLowerCase()}
-          key={navigationLink.name.toLocaleLowerCase()}
-        >
-          <img src={navigationLink.icon} />
-        </a>
-      ))}
+      <ul className={styles.navbarUl}>
+        {navigationLinks.map((navigationLink) => (
+          <li
+            className={styles.navBarLi}
+            key={navigationLink.name.toLocaleLowerCase()}
+          >
+            <a href={"#" + navigationLink.name.toLocaleLowerCase()}>
+              <img src={navigationLink.icon} />
+            </a>
+          </li>
+        ))}
+      </ul>
     </nav>
   );
 }
